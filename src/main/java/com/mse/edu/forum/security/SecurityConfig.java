bf0b6d967f36caa836183ed5f9aa1aafa358b868
Martin <47473742+martinzhelev@users.noreply.github.com>
2026-06-30 14:57:59 +0300

    FIX

--- src/main/java/com/mse/edu/forum/security/SecurityConfig.java
+++ src/main/java/com/mse/edu/forum/security/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
 				.requestMatchers("/error").permitAll()
 				.anyRequest()
 				.authenticated());
-		http.addFilterBefore(maintenanceModeFilter, JwtAuthenticationFilter.class);
+		http.addFilterBefore(maintenanceModeFilter, UsernamePasswordAuthenticationFilter.class);
 		http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
 	}
